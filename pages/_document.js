3788a604537f4717703bd057c47b0a342610e668
yagisuke <yagisuke.0209@gmail.com>
2019-04-02 01:07:11 +0900

    document

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import { getServersideToken, getUserScript } from '../lib/auth'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const props = await Document.getInitialProps(ctx)
+    const userData = await getServersideToken(ctx.req)
+
+    console.log('props', props)
+
+    return { ...props, ...userData }
+  }
+
+  render() {
+    const { user = {} } = this.props
+
+    return (
+      <html>
+        <Head />
+        <body>
+          <Main />
+          <script dangerouslySetInnerHTML={{ __html: getUserScript(user) }}></script>
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
